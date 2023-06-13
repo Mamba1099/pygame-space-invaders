501dd4fa8f8a13bb8c8f91796ecfe2568e0b78ab
Mamba1099 <mambakaranja240@gmail.com>
2023-06-13 16:15:13 +0300

    refactor: moved enemy class to it's own file

---
+++ game/enemy.py
@@ -0,0 +1,22 @@
+import pygame
+
+from game.ship import Ship
+
+from settings.ships import RED_SPACE_SHIP, GREEN_SPACE_SHIP, BLUE_SPACE_SHIP
+from settings.lasers import RED_LASER, GREEN_LASER, BLUE_LASER
+
+
+class Enemy(Ship):
+    COLOR_MAP = {
+        "red": (RED_SPACE_SHIP, RED_LASER),
+        "green": (GREEN_SPACE_SHIP, GREEN_LASER),
+        "blue": (BLUE_SPACE_SHIP, BLUE_LASER),
+    }
+
+    def __init__(self, x, y, color, health=100):
+        super().__init__(x, y, health)
+        self.ship_img, self.laser_img = self.COLOR_MAP[color]
+        self.mask = pygame.mask.from_surface(self.ship_img)
+
+    def move(self, vel):
+        self.y += vel
